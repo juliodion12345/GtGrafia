c65a52b0e0cdec95829c201ddb1584c8ae2963f2
Ibara Mayaka <juliodion_12345@hotmail.com>
2014-09-30 18:40:02 -0600

    Agregados metodos a la base de datos Modificada Evaluacion *TODO: Remover codigo de compatibilidad

--- GtGrafia/src/com/tesis/gtgrafia/EvaluacionActivity.java
+++ GtGrafia/src/com/tesis/gtgrafia/EvaluacionActivity.java
@@ -10,11 +10,22 @@
 
 package com.tesis.gtgrafia;
 
+import com.tesis.gtgrafia.estructura.Evaluacion;
+
 import android.app.Activity;
 import android.os.Bundle;
 
 public class EvaluacionActivity extends Activity {
-
+	
+	/**
+	 * Variable usada para almacenar el idUsuario de la aplicación
+	 */
+	private static int VAR_USUARIO = 0;
+	/**
+	 * Variable usada para almacenar el nivel a evaluar
+	 */
+	private static int VAR_IDNIVEL = 0;
+	
 	/**
 	 * Metodo que carga la pantalla de la evaluacion
 	 * 
@@ -26,5 +37,21 @@ public class EvaluacionActivity extends Activity {
 		setContentView(R.layout.activity_evaluacion);
 	}
 	
+	/**
+	 * Metodo que redirige las preguntas de la evaluacion
+	 */
+	public void realizarEvaluacion() {		
+		//Llenar evaluacion
+		Evaluacion eval = this.getEvaluacion();
+				
+		//Enviar evaluacion a Activity
+		
+	}
+	
+	public Evaluacion getEvaluacion() {
+		Evaluacion eval = new Evaluacion();	
+		
+		return eval;
+	}
 	
 }
--- GtGrafia/src/com/tesis/gtgrafia/SQLHelper.java
+++ GtGrafia/src/com/tesis/gtgrafia/SQLHelper.java
@@ -42,34 +42,147 @@ public class SQLHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS usuario_nivel");
         db.execSQL("DROP TABLE IF EXISTS evaluacion");
  
-        //Se crea la nueva versi髇 de la tablas
+        //Se crea la nueva versi锟絥 de la tablas
         onCreate(db);
     }
+    	
+	/**
+	 * Metodo que abre la base de datos
+	 */
+	public void abrir(){
+		this.getWritableDatabase();
+	}
     
-    //aqui se crean todos los metodos para acceder a la base de datos
+	/**
+	 * Metodo que cierra la base de datos
+	 */
+	public void cerrar(){
+		this.close();
+	}    
+	
+	/**
+	 * Metodo que devuelve un cursor con la consulta enviada
+	 * 
+	 * @param consulta Consulta a la base de datos
+	 * @return El cursor con la consulta realizada
+	 */    
+	public Cursor Select(String consulta){
+    	
+		//Cursor que obtiene la consulta
+		Cursor c = this.getReadableDatabase().rawQuery(consulta, null);
+		
+		//TODO: Analizar codigo
+		if (c!= null && c.getCount()>0){
+			c.moveToFirst();
+		}
+		
+		//Retorna el cursor
+		return c;		
+	}
     
+	/**
+	 * Metodo que inserta un usuario en la base de datos
+	 * 
+	 * @param nombre Nombre del usuario
+	 * @param punteo Punteo del usuario
+	 */ 
+	public void InsertUsuario (String nombre, int punteo){
+		//Iniciar la transaccion
+		this.getWritableDatabase().beginTransaction();
+		
+		//Crear el contenedor de valores
+		ContentValues valores = new ContentValues();
+		
+		//Insertar valores en el contenedor
+		valores.put("Nombre", nombre);
+		valores.put("Punteo", punteo);
+		
+		//Realizar la inserci贸n y verificar
+		if (this.getWritableDatabase().insert("usuario", null, valores) >=0){
+			
+			//Colocar la transacci贸n como exitosa
+			this.getWritableDatabase().setTransactionSuccessful();
+		}
+		
+		//Finalizar la transacci贸n
+		this.getWritableDatabase().endTransaction();
+	}
     
-    public void abrir(){
- 	   this.getWritableDatabase();
-    }
-    public void cerrar(){
- 	   this.close();
-    }
+	/**
+	 * Metodo que inserta un usuario_nivel en la base de datos
+	 * 
+	 * @param idUsuario ID del usuario
+	 * @param idNivel ID del nivel
+	 */ 
+	public void InsertUsuario_Nivel (int idUsuario, int idNivel){
+		//Iniciar la transaccion
+		this.getWritableDatabase().beginTransaction();
+		
+		//Crear el contenedor de valores
+		ContentValues valores = new ContentValues();
+		
+		//Insertar valores en el contenedor
+		valores.put("Usuario_id", idUsuario);
+		valores.put("idNivel", idNivel);
+		
+		//Realizar la inserci贸n y verificar
+		if (this.getWritableDatabase().insert("usuario_nivel", null, valores) >=0){
+			
+			//Colocar la transacci贸n como exitosa
+			this.getWritableDatabase().setTransactionSuccessful();
+		}
+		
+		//Finalizar la transacci贸n
+		this.getWritableDatabase().endTransaction();
+	}
+	
+	/**
+	 * Metodo que inserta una evaluacion en la base de datos
+	 * 
+	 * @param idEvaluacion ID de la evaluacion
+	 * @param idPregunta ID de la pregunta
+	 * @param idUsuario ID del usuario
+	 */ 
+	public void InsertEvaluacion (int idEvaluacion, int idPregunta, int idUsuario){
+		//Iniciar la transaccion
+		this.getWritableDatabase().beginTransaction();
+		
+		//Crear el contenedor de valores
+		ContentValues valores = new ContentValues();
+		
+		//Insertar valores en el contenedor
+		valores.put("idEvaluacion", idEvaluacion);
+		valores.put("idPregunta", idPregunta);
+		valores.put("Usuario_id", idUsuario);
+		
+		//Realizar la inserci贸n y verificar
+		if (this.getWritableDatabase().insert("evaluacion", null, valores) >=0){
+			
+			//Colocar la transacci贸n como exitosa
+			this.getWritableDatabase().setTransactionSuccessful();
+		}
+		
+		//Finalizar la transacci贸n
+		this.getWritableDatabase().endTransaction();
+	}	
     
+	//TODO: Remover despues    
     public Cursor SelectUsuario(){
- 	   Cursor c = this.getReadableDatabase().rawQuery("select * from usuario", null);
- 	   if (c!= null && c.getCount()>0){
- 		   c.moveToFirst();
- 	   }
- 	   return c;
-    }
+  	   Cursor c = this.getReadableDatabase().rawQuery("select * from usuario", null);
+  	   if (c!= null && c.getCount()>0){
+  		   c.moveToFirst();
+  	   }
+  	   return c;
+     }
+    
+	//TODO: Remover despues
     public void InsertUsuario (int id, String nombre){
- 	   this.getWritableDatabase().beginTransaction();
- 	   ContentValues valores = new ContentValues();
- 	   valores.put("Nombre", nombre);
- 	   if (this.getWritableDatabase().insert("usuario", null, valores) >=0){
- 		   this.getWritableDatabase().setTransactionSuccessful();
- 	   }
- 	   this.getWritableDatabase().endTransaction();
-    }
+  	   this.getWritableDatabase().beginTransaction();
+  	   ContentValues valores = new ContentValues();
+  	   valores.put("Nombre", nombre);
+  	   if (this.getWritableDatabase().insert("usuario", null, valores) >=0){
+  		   this.getWritableDatabase().setTransactionSuccessful();
+  	   }
+  	   this.getWritableDatabase().endTransaction();
+     }
 }
