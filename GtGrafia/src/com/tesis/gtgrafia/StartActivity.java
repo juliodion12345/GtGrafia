ba8c19f2024695aca80bc87808213923885e8717
Ibara Mayaka <juliodion_12345@hotmail.com>
2014-10-31 17:28:22 -0600

    Aplicación funcional

--- GtGrafia/src/com/tesis/gtgrafia/StartActivity.java
+++ GtGrafia/src/com/tesis/gtgrafia/StartActivity.java
@@ -5,8 +5,6 @@ import com.tesis.gtgrafia.usuario.UsuarioActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
